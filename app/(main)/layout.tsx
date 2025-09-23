a96dd8b2e1b622862114633b99c80f44d9b5dce7
debabin <dmitriybabinup@gmail.com>
2025-09-23 18:55:46 +0700

    add db

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,8 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { AnimatedShinyText, DotPattern, HyperText } from '@/components/ui';
-import { prisma } from '@/lib/prisma';
-import { cn } from '@/lib/utils';
+import { AnimatedShinyText, DotPattern, HyperText } from "@/components/ui";
+import { prisma } from "@/lib/prisma";
+import { cn } from "@/lib/utils";
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -11,15 +11,15 @@ interface MainLayoutProps {
 const MainLayout = async ({ children }: MainLayoutProps) => {
   const recentTalks = await prisma.talk.findMany({
     take: 3,
-    orderBy: { updatedAt: 'desc' }
+    orderBy: { updatedAt: "desc" },
   });
 
   return (
-    <div className='relative min-h-screen overflow-hidden bg-white transition-colors dark:bg-black'>
+    <div className="relative min-h-screen overflow-hidden bg-white transition-colors dark:bg-black">
       <DotPattern
         className={cn(
-          'fill-gray-400/20 dark:fill-white/10',
-          '[mask-image:radial-gradient(700px_circle_at_center,white,transparent)]'
+          "fill-gray-400/20 dark:fill-white/10",
+          "[mask-image:radial-gradient(700px_circle_at_center,white,transparent)]"
         )}
         cr={1}
         cx={1}
@@ -28,27 +28,27 @@ const MainLayout = async ({ children }: MainLayoutProps) => {
         width={20}
       />
 
-      <div className='relative z-10 flex min-h-screen flex-col items-center justify-center px-4 sm:px-8'>
+      <div className="relative z-10 flex min-h-screen flex-col items-center justify-center px-4 sm:px-8">
         <div
           className={cn(
-            'group rounded-full border transition-all ease-in hover:cursor-pointer',
-            'border-gray-200 bg-gray-100 text-gray-800 hover:bg-gray-200',
-            'dark:border-white/10 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-800'
+            "group rounded-full border transition-all ease-in hover:cursor-pointer",
+            "border-gray-200 bg-gray-100 text-gray-800 hover:bg-gray-200",
+            "dark:border-white/10 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-800"
           )}
         >
-          <AnimatedShinyText className='inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-gray-600 hover:duration-300 dark:hover:text-neutral-400'>
+          <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-gray-600 hover:duration-300 dark:hover:text-neutral-400">
             <span>✨ introducing speakers</span>
           </AnimatedShinyText>
         </div>
 
-        <div className='mx-auto max-w-3xl space-y-12 text-center'>
-          <div className='flex flex-col items-center justify-center gap-2'>
-            <h1 className='text-6xl font-bold text-gray-900 transition-colors dark:text-white'>
+        <div className="mx-auto max-w-3xl space-y-12 text-center">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <h1 className="text-6xl font-bold text-gray-900 transition-colors dark:text-white">
               Easy to generate information about conferences
             </h1>
 
             <HyperText
-              className='text-sm font-light text-gray-600 transition-colors dark:text-gray-400'
+              className="text-sm font-light text-gray-600 transition-colors dark:text-gray-400"
               animateOnHover={false}
               duration={1000}
             >
@@ -60,10 +60,13 @@ const MainLayout = async ({ children }: MainLayoutProps) => {
         </div>
 
         {!!recentTalks.length && (
-          <div className='mt-2 flex flex-col items-center justify-center gap-1'>
+          <div className="mt-2 flex flex-col items-center justify-center gap-1">
             {recentTalks.map((talk) => (
-              <div key={talk.id} className='text-xs text-gray-600 dark:text-gray-400'>
-                <Link href={`/analysis/${talk.id}`} className='underline'>
+              <div
+                key={talk.id}
+                className="text-xs text-gray-600 dark:text-gray-400"
+              >
+                <Link href={`/analysis/${talk.id}`} className="underline">
                   {talk.speaker} - {talk.title}
                 </Link>
               </div>
@@ -71,14 +74,14 @@ const MainLayout = async ({ children }: MainLayoutProps) => {
           </div>
         )}
 
-        <div className='mt-5 text-xs text-gray-600 dark:text-gray-400'>
+        <div className="mt-5 text-xs text-gray-600 dark:text-gray-400">
           <span>
-            made by{' '}
+            made by{" "}
             <a
-              href='https://github.com/siberiacancode'
-              className='underline'
-              rel='noopener noreferrer'
-              target='_blank'
+              href="https://github.com/siberiacancode"
+              className="underline"
+              rel="noopener noreferrer"
+              target="_blank"
             >
               siberiacancode
             </a>
--- app/analysis/(helpers)/parsers/jugru.ts
+++ app/analysis/(helpers)/parsers/jugru.ts
@@ -1,22 +1,25 @@
-import { JSDOM } from 'jsdom';
+import { JSDOM } from "jsdom";
+import { htmlToMarkdown } from "../../(actions)";
 
 export const parseJugru = async (url: string, html: string) => {
   const dom = new JSDOM(html);
   const doc = dom.window.document;
 
   const titleElement = doc.querySelector('[class*="talkContent__heading"]');
-  const descriptionElement = doc.querySelector('[class*="talkContent__description"]');
+  const descriptionElement = doc.querySelector(
+    '[class*="talkContent__description"]'
+  );
   const speakerElement = doc.querySelector('[class*="personCard__name"]');
   const companyElement = doc.querySelector('[class*="personCard__company"]');
   const avatarElement = doc.querySelector('img[class*="avatar__image"]');
   const logoElement = doc.querySelector('img[class*="footer__logo"]');
 
   const title = titleElement!.textContent!.trim();
-  const description = descriptionElement!.innerHTML;
+  const description = await htmlToMarkdown(descriptionElement!.innerHTML);
   const speaker = speakerElement!.textContent!.trim();
   const company = companyElement!.textContent!.trim();
-  const speakerAvatar = avatarElement!.getAttribute('src');
-  const logo = logoElement!.getAttribute('src');
+  const speakerAvatar = avatarElement!.getAttribute("src");
+  const logo = logoElement!.getAttribute("src");
 
   return {
     title,
@@ -25,6 +28,6 @@ export const parseJugru = async (url: string, html: string) => {
     company,
     description,
     logo,
-    url
+    url,
   };
 };
