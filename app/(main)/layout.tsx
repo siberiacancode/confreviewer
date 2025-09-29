cf3ad50ca911f2238254010af2d54b7f3b5134ba
debabin <dmitriybabinup@gmail.com>
2025-09-30 00:26:24 +0700

    add select for talks and api endpoint

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,35 +1,33 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { AnimatedShinyText, DotPattern, HyperText } from '@/components/ui';
-import { cn } from '@/lib/utils';
+import { AnimatedShinyText, DotPattern, HyperText } from "@/components/ui";
+import { cn } from "@/lib/utils";
 
-import type { SearchResponse } from '../api/search/route';
+import type { SearchResponse } from "../api/search/route";
 
-import { api } from '../api/instance';
+import { api } from "../api/instance";
 
 interface MainLayoutProps {
   children: React.ReactNode;
 }
 
-export const revalidate = 10;
-
 const MainLayout = async ({ children }: MainLayoutProps) => {
-  const searchResponse = await api.get<SearchResponse>('/search', {
+  const searchResponse = await api.get<SearchResponse>("/search", {
     query: {
       limit: 3,
-      search: ''
+      search: "",
     },
     next: {
-      revalidate: 10
-    }
+      revalidate: 10,
+    },
   });
 
   return (
-    <div className='relative min-h-screen overflow-hidden bg-white transition-colors dark:bg-black'>
+    <div className="relative min-h-screen overflow-hidden bg-white transition-colors dark:bg-black">
       <DotPattern
         className={cn(
-          'fill-gray-400/20 dark:fill-white/10',
-          '[mask-image:radial-gradient(700px_circle_at_center,white,transparent)]'
+          "fill-gray-400/20 dark:fill-white/10",
+          "[mask-image:radial-gradient(700px_circle_at_center,white,transparent)]"
         )}
         cr={1}
         cx={1}
@@ -38,27 +36,27 @@ const MainLayout = async ({ children }: MainLayoutProps) => {
         width={20}
       />
 
-      <div className='relative z-10 flex min-h-screen flex-col items-center justify-center px-4 sm:px-8'>
+      <div className="relative z-10 flex min-h-screen flex-col items-center justify-center px-4 sm:px-8">
         <div
           className={cn(
-            'group rounded-full border transition-all ease-in hover:cursor-pointer',
-            'border-gray-200 bg-gray-100 text-gray-800 hover:bg-gray-200',
-            'dark:border-white/10 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-800'
+            "group rounded-full border transition-all ease-in hover:cursor-pointer",
+            "border-gray-200 bg-gray-100 text-gray-800 hover:bg-gray-200",
+            "dark:border-white/10 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-800"
           )}
         >
-          <AnimatedShinyText className='inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-gray-600 hover:duration-300 dark:hover:text-neutral-400'>
+          <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-gray-600 hover:duration-300 dark:hover:text-neutral-400">
             <span>✨ introducing speakers</span>
           </AnimatedShinyText>
         </div>
 
-        <div className='mx-auto max-w-3xl space-y-12 text-center'>
-          <div className='flex flex-col items-center justify-center gap-2'>
-            <h1 className='text-6xl font-bold text-gray-900 transition-colors dark:text-white'>
+        <div className="mx-auto max-w-3xl space-y-12 text-center">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <h1 className="text-6xl font-bold text-gray-900 transition-colors dark:text-white">
               Easy to generate information about conferences
             </h1>
 
             <HyperText
-              className='text-sm font-light text-gray-600 transition-colors dark:text-gray-400'
+              className="text-sm font-light text-gray-600 transition-colors dark:text-gray-400"
               animateOnHover={false}
               duration={1000}
             >
@@ -70,10 +68,13 @@ const MainLayout = async ({ children }: MainLayoutProps) => {
         </div>
 
         {!!searchResponse.data.talks.length && (
-          <div className='mt-4 flex flex-col items-center justify-center gap-1'>
+          <div className="mt-4 flex flex-col items-center justify-center gap-1">
             {searchResponse.data.talks.map((talk) => (
-              <div key={talk.id} className='text-xs text-gray-600 dark:text-gray-400'>
-                <Link href={`/analysis/${talk.id}`} className='underline'>
+              <div
+                key={talk.id}
+                className="text-xs text-gray-600 dark:text-gray-400"
+              >
+                <Link href={`/analysis/${talk.id}`} className="underline">
                   {talk.speaker} - {talk.title}
                 </Link>
               </div>
@@ -81,14 +82,14 @@ const MainLayout = async ({ children }: MainLayoutProps) => {
           </div>
         )}
 
-        <div className='mt-5 text-xs text-gray-600 dark:text-gray-400'>
+        <div className="mt-5 text-xs text-gray-600 dark:text-gray-400">
           <span>
-            made by{' '}
+            made by{" "}
             <a
-              href='https://github.com/siberiacancode'
-              className='underline'
-              rel='noopener noreferrer'
-              target='_blank'
+              href="https://github.com/siberiacancode"
+              className="underline"
+              rel="noopener noreferrer"
+              target="_blank"
             >
               siberiacancode
             </a>
